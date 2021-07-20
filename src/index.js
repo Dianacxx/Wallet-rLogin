570da1f602b9880ffaf5832e466c5ce01b2479b8
david-lagos <81312863+david-lagos@users.noreply.github.com>
2021-07-20 10:39:29 -0500

    add comments for clarity

--- src/index.js
+++ src/index.js
@@ -12,22 +12,28 @@ window.onload = function() {
     // Functions
     const connect = async() => {
         try {
+            // find provider
             let provider = window.ethereum;
             if(provider) {
+                // send connection request to wallet
                 await provider.request({ method: 'eth_requestAccounts' });
                 
+                // use web3 to fetch array of accounts
                 web3 = new Web3(provider);
 
                 web3.eth.getAccounts().then(addresses => {
                     account.innerText = addresses[0];
                 });
-
+                
+                // display account information and hidde connectButton
                 content.style.display = '';
                 connectButton.style.display = 'none';
             } else {
+                // handler in case a web3 provider is not found
                 alert('You require a web3 provider');
             }
         } catch(error) {
+            // handler in case user rejects the connection request
             alert('You have rejected the request');
         }
     }
