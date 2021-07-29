dc6f8aac87b19fdc6823178baf46a9e128d17b2a
Jhon Garces Montes <jairogarces1@gmail.com>
2021-07-29 15:25:21 -0500

    Files to clone

--- src/App.js
+++ src/App.js
@@ -1,44 +1,24 @@
-import RLogin, { RLoginButton } from '@rsksmart/rlogin'
-import WalletConnectProvider from '@walletconnect/web3-provider'
-import { useState } from 'react';
+import React from 'react';
 import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Slider from './components/Slider'
 
 
-// construct rLogin pop-up in DOM
-const rLogin = new RLogin({
-    cachedProvider: false, // change to true to cache user's wallet choice
-    providerOptions: { 
-      walletconnect: {
-        package: WalletConnectProvider, // setup wallet connect for mobile wallet support
-        options: {
-          rpc: {
-            31: 'https://public-node.testnet.rsk.co' // use RSK public nodes to connect to the testnet
-          }
-        }
-      }
-    },
-    supportedChains: [31] // enable rsk testnet network
-  })
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header></Header>
+      <Slider/>
+      <Home/>
+      <Footer></Footer>
+    </div>
+  );
+}
+
   
-  function App() {
-    const [provider, setProvider] = useState(null)
-    const [account, setAccount] = useState(null)
-  
-      // display pop up
-    const connect = () => rLogin.connect()
-      .then(({ provider }) => { 
-        setProvider(provider)
-        // request user's account
-        provider.request({ method: 'eth_accounts' }).then(([account]) => setAccount(account))
-      })
-  
-    return (
-      <div className="App">
-        <RLoginButton onClick={connect}>Connect wallet</RLoginButton>
-        <p>wallet address: {account}</p>
-      </div>
-    );
-  }
-  
-  export default App;
+export default App;
   
---
+++ src/components/Slider.js
@@ -0,0 +1,114 @@
+import React from "react";
+
+function Slider() {
+  return (
+    <div class="container-slider">
+        <h1 class="slider-title">Top Coins</h1>
+      <div class="slider consumer-logos">
+        <div class="slide-track">
+          {/* <!-- 9 slide --> */}
+          <div class="slide">
+            <div class="stock">
+              <img src="img/btc.png" alt="bitcoin-logo" />
+              <p>BTC</p>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="stock">
+              <img src="img/eth.png" alt="" />
+              <p>ETH</p>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="stock">
+              <img src="img/usdt.png" alt="" />
+              <p>USDT</p>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="stock">
+              <img src="img/bnb.png" alt="" />
+              <p>BNB</p>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="stock">
+              <img src="img/ada.png" alt="" />
+              <p>ADA</p>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="stock">
+              <img src="img/btc.png" alt="" />
+              <p>BTC</p>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="stock">
+              <img src="img/usdt.png" alt="" />
+              <p>USDT</p>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="stock">
+              <img src="img/eth.png" alt="" />
+              <p>ETH</p>
+            </div>
+          </div>
+
+          {/* <!-- 9 Slide DOUBLE --> */}
+          <div class="slide">
+            <div class="stock">
+              <img src="img/btc.png" alt="bitcoin-logo" />
+              <p>BTC</p>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="stock">
+              <img src="img/eth.png" alt="" />
+              <p>ETH</p>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="stock">
+              <img src="img/usdt.png" alt="" />
+              <p>USDT</p>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="stock">
+              <img src="img/bnb.png" alt="" />
+              <p>BNB</p>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="stock">
+              <img src="img/ada.png" alt="" />
+              <p>ADA</p>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="stock">
+              <img src="img/btc.png" alt="" />
+              <p>BTC</p>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="stock">
+              <img src="img/usdt.png" alt="" />
+              <p>USDT</p>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="stock">
+              <img src="img/eth.png" alt="" />
+              <p>ETH</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Slider;
